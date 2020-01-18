13aeccf7fb920a47bf097247aa0693013d8eaa86
Rakib <rakib9204@gmail.com>
2020-01-18 10:31:46 +0600

    first comment

---
+++ app/Http/Controllers/Api/ClassController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+class ClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $class=DB::table('classes')->get();
+        return response()->json($class);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData=$request->validate([
+          'class_name'=>'required|unique:classes|max:25'
+        ]);
+        $data=array();
+        $data['class_name']=$request->class_name;
+        DB::table('classes')->insert($data);
+        return response('done');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $var=DB::table('classes')->where('class_id',$id)->first();
+      return response()->json($var);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      DB::table('classes')->where('class_id',$id)->delete();
+      return response('deleted');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::apiResource('/class','Api\ClassController');
+Route::apiResource('/subject','Api\SubjectController');
+Route::apiResource('/student','Api\StudentController');
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
